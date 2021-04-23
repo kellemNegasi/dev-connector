90cfb934964183922cdb9915a13acc89a042b0f3
kellemnegasi <kellemnegasi@gmail.com>
2021-04-23 16:52:37 +0300

    installed and set up react router

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,24 @@ import './App.css';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <Landing/>
-      <Footer/>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <div className="container">
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component = {Login}/>
+        </div>
+        <Footer />
+        
     </div>
+    </Router>
+  
   );
 }
 
