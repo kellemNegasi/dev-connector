b4b4a72b7cff368f282ff77a8980fce0df516512
kellemnegasi <kellemnegasi@gmail.com>
2021-05-04 00:09:01 +0300

    edited login button

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -80,7 +80,10 @@ class Login extends Component {
                   onChange={this.onChange}
                   error={errors.password}
                 />
-                <input type="submit" className="btn btn-info btn-block mt-4" />
+                <input
+                  type="submit"
+                  className="btn btn btn-primary btn-lg active mt-4"
+                />
               </form>
             </div>
           </div>
