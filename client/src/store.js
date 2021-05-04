d0d34fb77a15c4876ea0d428f162dec8d7b97cf4
kellemnegasi <kellemnegasi@gmail.com>
2021-05-05 00:16:21 +0300

    fixed redux issue in production mode

--- client/src/store.js
+++ client/src/store.js
@@ -1,15 +1,17 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
 const initialState = {};
 const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
+  composeWithDevTools(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__({ trace: true, traceLimit: 25 })
+    (window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__({ trace: true, traceLimit: 25 })) ||
+      compose
   )
 );
 export default store;
