622bee0137be495cf036bec1f0de36d43bd62e38
kellemnegasi <kellemnegasi@gmail.com>
2021-05-05 00:29:59 +0300

    fixed small redux extension issue

--- client/src/store.js
+++ client/src/store.js
@@ -1,13 +1,12 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
-import { composeWithDevTools } from "redux-devtools-extension";
 const initialState = {};
 const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  composeWithDevTools(
+  compose(
     applyMiddleware(...middleware),
     (window.__REDUX_DEVTOOLS_EXTENSION__ &&
       window.__REDUX_DEVTOOLS_EXTENSION__({ trace: true, traceLimit: 25 })) ||
